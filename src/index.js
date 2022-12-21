05ab8417d9e9ea24b899fdd1bde5c682d1d645f4
Donald Akite <donaldakite27@gmail.com>
2022-12-21 16:40:21 +0000

    Add ui

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './style.css';
 import './modules/send.js';
+import getResponse from './modules/getResponse.js';
+
+const Display = document.querySelector('.score-update');
+const GetData = document.getElementById('refresh-btn');
+
+GetData.addEventListener('click', async () => {
+  const scoresUpdate = await getResponse();
+  Display.innerHTML = '';
+  scoresUpdate.sort((High, Low) => Low.score - High.score).forEach((scores) => {
+    Display.innerHTML += `
+    <div class="name-score">
+        <p>${scores.user}: </p>
+        <p>${scores.score}</p>
+    </div>
+    `;
+  });
+});
