cf77e56c56335471083f282c7f3fb9d58d4d5a0b
Donald Akite <donaldakite27@gmail.com>
2023-01-08 13:28:09 +0000

    Update file names

--- src/modules/getResponse.js
+++ src/modules/getResponse.js
@@ -1,7 +1,7 @@
 const getResponse = async () => {
   try {
-    const AvatarURL = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BSY5VexV1P3xkitZrGsn/scores');
-    const response = await AvatarURL.json();
+    const avatarURL = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BSY5VexV1P3xkitZrGsn/scores');
+    const response = await avatarURL.json();
     return response.result;
   } catch (error) {
     return error;
