f271dd0395ad2e101cda1b029ece1bf625a0291c
Donald Akite <donaldakite27@gmail.com>
2023-02-27 20:59:02 +0000

    Add function to fetch scores when page loads

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import './style.css';
-import './modules/Send.js';
-import getResponse from './modules/GetResponse.js';
+import getResponse from './modules/getResponse.js';
 
 const Display = document.querySelector('.score-update');
 const GetData = document.getElementById('refresh-btn');
+const alert = document.querySelector('alert');
 
 GetData.addEventListener('click', async () => {
   const scoresUpdate = await getResponse();
   Display.innerHTML = '';
+  scoresUpdate.sort((High, Low) => Low.score - High.score).forEach((scores) => {
+    Display.innerHTML += `
+    <div class="name-score">
+        <p>${scores.user}: </p>
+        <p>${scores.score}</p>
+    </div>
+    `;
+  });
+});
+
+window.onload = (async () => {
+  const scoresUpdate = await getResponse();
+  if (getResponse) {
+    alert.innerText = '';
+  }
   scoresUpdate.sort((High, Low) => Low.score - High.score).forEach((scores) => {
     Display.innerHTML += `
     <div class="name-score">
